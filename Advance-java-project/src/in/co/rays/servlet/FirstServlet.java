1bb3dd55cc7cf0e0881c9a2e98f5fc9f47556743
rish1208 <rishd01wivedi@gmail.com>
2024-04-12 16:30:10 +0530

    done

---
+++ Advance-java-project/src/in/co/rays/servlet/FirstServlet.java
@@ -0,0 +1,43 @@
+package in.co.rays.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("FirstServlet doGet");
+
+		// resp.sendRedirect("SecondServlet");
+
+		RequestDispatcher rd = req.getRequestDispatcher("SecondServlet");
+		rd.forward(req, resp);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("FirstServlet doPost");
+
+		System.out.println("FirstServlet firstName = " + req.getParameter("firstName"));
+
+         req.setAttribute("lastName", "xyz");
+         req.setAttribute("loginID", "abc@gmail.com");
+
+		// resp.sendRedirect("SecondServlet");
+
+		RequestDispatcher rd = req.getRequestDispatcher("SecondServlet");
+		rd.forward(req, resp);
+
+	}
+
+}
